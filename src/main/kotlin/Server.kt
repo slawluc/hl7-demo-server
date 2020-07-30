f670d415c9da291006d52498dcc499dd3722959a
slawluc <stuart.law@lucaya.net>
2020-07-30 21:02:47 +0100

    Updates kotlin version

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -8,26 +8,25 @@ import ca.uhn.hl7v2.protocol.ReceivingApplicationExceptionHandler
 import java.io.IOException
 
 
+/**
+ * http://dev.patientsknowbest.com/home/data-model#TOC-Lab-Report
+ * http://dev.patientsknowbest.com/home/hl7-api/messages/oru-r01/laboratory
+ * https://hapifhir.github.io/hapi-hl7v2/v281/apidocs/ca/uhn/hl7v2/model/v281/message/ORU_R01.html
+ * https://hapifhir.github.io/hapi-hl7v2/v281/apidocs/index.html
+ * https://wiki.openmrs.org/display/docs/HL7
+ */
 fun main() {
     println("Starting")
-
     val handler: ReceivingApplication<Message> = ExampleReceiverApplication()
 
     DefaultHapiContext().newServer(Config.port, Config.useTls).apply {
         registerApplication("ADT", "A01", handler)
         registerApplication("ADT", "A02", handler)
         registerApplication("ORU", "R01", handler)
-        //http://dev.patientsknowbest.com/home/data-model#TOC-Lab-Report
-        //http://dev.patientsknowbest.com/home/hl7-api/messages/oru-r01/laboratory
-        //https://hapifhir.github.io/hapi-hl7v2/v281/apidocs/ca/uhn/hl7v2/model/v281/message/ORU_R01.html
-        //https://hapifhir.github.io/hapi-hl7v2/v281/apidocs/index.html
-        //https://wiki.openmrs.org/display/docs/HL7
-
         registerConnectionListener(MyConnectionListener())
         setExceptionHandler(MyExceptionHandler())
         startAndWait()
     }
-
 }
 
 class ExampleReceiverApplication : ReceivingApplication<Message> {
